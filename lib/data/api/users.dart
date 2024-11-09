0891492fbd71f58a8f14bebfe00955062eab82e9
armandonunez404 <armandonunez404@gmail.com>
2024-11-09 10:22:04 -0400

    refactor

--- lib/data/api/users.dart
+++ lib/data/api/users.dart
@@ -28,37 +28,5 @@ class UserApi {
   }
 
 
- Future<List<Pet>> fetchAvailablePets( int? especieId ) async {
-    List<Pet> pets = [];
-     
-    final url =(especieId != 0 && especieId != null)
-        ? '$backendUrl/publicaciones/disponibles?especie=$especieId'
-        : '$backendUrl/publicaciones/disponibles';
-
-    final response = await http.get(
-      Uri.parse(url),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    );
-
-    final body = jsonDecode(response.body);
-    Logger().i("Respuesta: $body");
-    if (body['transaction']) {
-      List<dynamic> dataJson = body['data'];
-      pets = dataJson.map((dynamic item) {
-        try {
-          return Pet.fromJson(item);
-        } catch (e) {
-          // Manejar el error de parsing
-          return null;
-        }
-      }).whereType<Pet>().toList();
-    } else {
-      throw Exception(body['message']);
-    }
-
-    return pets;
-  }
 
 }
